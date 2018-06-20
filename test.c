0220d42fc4ab7b925016c3b10ce9828479887549
lxy6277 <lxy6277@163.com>
2018-06-20 17:45:37 +0800

    first

---
+++ test.c
@@ -0,0 +1,27 @@
+
+//
+
+
+#include <reg52.h>
+
+sbit LED=P2^1;
+
+void delay(unsigned int a)
+
+{
+	unsigned int c;
+	for(c=0;c<a;c++)
+	;
+}
+	
+void main()
+{
+		
+	while(1)
+	{
+		LED=1;
+		delay(200);
+		LED=0;
+		delay(200);
+	}
+}
